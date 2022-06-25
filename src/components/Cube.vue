0160c253f67414407b29a9e6aeee7f1335080ef8
ypp <paopao0108@outlook.com>
2022-06-25 11:06:08 +0800

    优化单个魔方旋转

--- src/components/Cube.vue
+++ src/components/Cube.vue
@@ -13,11 +13,9 @@
           height: side + 'px',
           transform: `translateX(${side * block.x}px)  translateY(${side * block.y}px) translateZ(${side * block.z}px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)`,
           '--size': `${side}px`,
-          'transform-origin': `${origins[i].originx} ${origins[i].originy} ${origins[i].originz}`
+          'transform-origin': `${block.origin.originx} ${block.origin.originy} ${block.origin.originz}`
         }"
       >
-        <!-- :style="{ width: side + 'px', height: side + 'px', transform: 'translateX(' + side * block.x + 'px)  translateY(' + side * block.y + 'px) translateZ(' + side * block.z + 'px)', '--size': side + 'px' }" -->
-        <!-- {{ block.x }}{{ block.y }}{{ block.z }} -->
         <div class="front">{{ block.x }}{{ block.y }}{{ block.z }}</div>
         <div class="back">{{ block.x }}{{ block.y }}{{ block.z }}</div>
         <div class="left">{{ block.x }}{{ block.y }}{{ block.z }}</div>
@@ -31,14 +29,12 @@
 
 <script>
 export default {
-  props: ['x', 'y', 'z'],
   name: 'cube',
   data() {
     return {
       count: 3,
       side: 50,
-      blocks: [],
-      origins: []
+      blocks: []
     }
   },
   created() {
@@ -47,59 +43,28 @@ export default {
   methods: {
     // 创建魔方块
     createBlocks() {
-      let originx = ''
-      let originy = ''
-      let originz = ''
       for (let x = 0; x < this.count; x++) {
-        if (x === 0) {
-          originx = '75px'
-        } else if (x === 1) {
-          originx = '25px'
-        } else {
-          originx = '-25px'
-        }
         for (let y = 0; y < this.count; y++) {
-          if (y === 0) {
-            originy = '75px'
-          } else if (y === 1) {
-            originy = '25px'
-          } else {
-            originy = '-25px'
-          }
           for (let z = 0; z < this.count; z++) {
-            if (z === 0) {
-              originz = '25px'
-            } else if (z === 1) {
-              originz = '-25px'
-            } else {
-              originz = '-75px'
-            }
-            // origin 用于存放每个魔方块的transform-origin的值
-            const origin = {
-              originx,
-              originy,
-              originz
-            }
-            console.log(origin)
-            // block用于存放当前块的坐标
             const block = {
               x,
               y,
               z,
-              origin
+              // origin 用于存放每个魔方块的transform-origin的值
+              origin: {
+                originx: 75 - 50 * x + 'px',
+                originy: 75 - 50 * y + 'px',
+                originz: 25 - 50 * z + 'px'
+              }
             }
             // 将生成的坐标块放入blocks中
             this.blocks.push(block)
-            this.origins.push(origin)
           }
         }
       }
       console.log(this.blocks)
-    },
-    getOrigin() {}
-  },
-  // 计算属性
-  computed: {}
+    }
+  }
 }
 </script>
 
@@ -123,7 +88,6 @@ export default {
     transform: rotateX(-30deg) rotateY(145deg) rotateZ(0deg);
     .block {
       position: absolute;
-      // border: 10px solid purple;
       box-sizing: border-box;
       transform-style: preserve-3d;
       div {
